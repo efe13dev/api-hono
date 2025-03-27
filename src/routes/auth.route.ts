dcb3a1d7bf00e08b33d1ec9d77abdc21a590c668
efe <efe13dev@gmail.com>
2025-03-27 12:25:29 +0100

    feat(auth): enhance authentication routes
    - Improve register endpoint to return new user data
    - Implement login endpoint with password verification
    - Refactor variable names for better clarity

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -13,12 +13,12 @@ authRouter.post('/register', zValidator('json', registerSchema), async (c) => {
 	const { email, password, username } = c.req.valid('json');
 
 	// 3. Verificar que el usuario no existe
-	const [existingUser] = await db
+	const [user] = await db
 		.select()
 		.from(usersTable)
 		.where(eq(usersTable.email, email));
 
-	if (existingUser) {
+	if (user) {
 		return c.json({ message: 'The email is already registered' }, 409);
 	}
 
@@ -26,21 +26,44 @@ authRouter.post('/register', zValidator('json', registerSchema), async (c) => {
 	const hashedPassword = await Bun.password.hash(password);
 
 	// 5. Guardar el usuario en la base de datos
-	await db.insert(usersTable).values({
-		email,
-		password: hashedPassword,
-		username,
-	});
+	const [newUser] = await db
+		.insert(usersTable)
+		.values({
+			email,
+			password: hashedPassword,
+			username,
+		})
+		.returning({
+			id: usersTable.id,
+			username: usersTable.username,
+			email: usersTable.email,
+		});
 	// 6. Enviar una respuesta
-	return c.json({ message: `User ${username} registered successfully` }, 201);
+	return c.json({ newUser }, 201);
 });
 
-//1. Obtener los datos del usuario del body
-// 2. Validar los datos del usuario
-// 3. Verificar si el usario existe en la base de datos
-// 4. Verificar que la contraseña sea correcta
-// 5. Generar un token de autenticacion JWT
-// 6. Devolver el token
 authRouter.post('/login', zValidator('json', loginSchema), async (c) => {
+	// 1. Obtener los datos del usuario del body
+	// 2. Validar los datos del usuario
+	const { email, password } = c.req.valid('json');
+
+	// 3. Verificar si el usario existe en la base de datos
+	const [user] = await db
+		.select()
+		.from(usersTable)
+		.where(eq(usersTable.email, email));
+
+	if (!user) {
+		return c.json({ message: 'User or password is incorrect' }, 404);
+	}
+
+	// 4. Verificar que la contraseña sea correcta
+	const isPasswordValid = await Bun.password.verify(password, user.password);
+
+	if (!isPasswordValid) {
+		return c.json({ message: 'User or password is incorrect' }, 404);
+	}
+	// 5. Generar un token de autenticacion JWT
+	// 6. Devolver el token
 	return c.json({ message: 'Login' });
 });
